67e8af2bf2e640c57f12b45822516f717dfce78a
joaoppneves <joaodasneves@gmail.com>
2017-04-04 16:17:23 -0300

    Criação do menu hamburger animado

--- js/scripts.js
+++ js/scripts.js
@@ -217,6 +217,20 @@ zoom: {
     }()); 
 
 
+   // ----------------------------------------------
+    // Hamburger
+    // ----------------------------------------------
+
+    $(document).ready(function(){
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+        $(this).toggleClass('open');
+    });
+    $('#st-navbar-collapse').click(function(){
+        $('#nav-icon4').removeClass('open');
+    });
+});
+
+
     
 
 });
