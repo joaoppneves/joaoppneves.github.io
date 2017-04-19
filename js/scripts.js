38ac1c43e65c73a68a3a3e7a397b8d356c276ab6
joaoppneves <joaodasneves@gmail.com>
2017-04-19 14:47:58 -0300

    script.js

--- js/scripts.js
+++ js/scripts.js
@@ -222,15 +222,27 @@ zoom: {
     // ----------------------------------------------
 
     $(document).ready(function(){
-    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-        $(this).toggleClass('open');
+        $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+            $(this).toggleClass('open');
+        });
+        $('#st-navbar-collapse').click(function(){
+            $('#nav-icon4').removeClass('open');
+        });
     });
-    $('#st-navbar-collapse').click(function(){
-        $('#nav-icon4').removeClass('open');
-    });
-});
+
+    // ----------------------------------------------
+    // ANALYTICS
+    // ----------------------------------------------
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-97672144-1', 'auto');
+  ga('send', 'pageview');
 
 
- 
+  
 
 });
